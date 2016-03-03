ecd73992963b747f63c6bcf241a7ede74a07d63b
MatveyD <matv1995@gmail.com>
2016-03-04 01:33:17 +0300

    first commit

---
+++ WebApplication/Models/WeapModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class WeapModels
+    {
+        public String WeaponName { get; set; }
+        public String WeaponType { get; set; }
+        public String WeaponMade { get; set; }
+        public String WeaponRate { get; set; }
+        public String WeaponPrice { get; set; }
+        public String PicUrl { get; set; }
+    }
+}
